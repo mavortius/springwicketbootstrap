1f46d290ae5486f5b4050fa51c3dadd91cab43be
Marcelo Martins <m.a.martins@live.com>
2016-11-18 18:41:10 -0200

    Initial Commit.

--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -1,14 +1,19 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WicketApplication {
+public class WicketApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WicketApplication.class);
+	}
 
 	public static void main(String[] args) throws Exception {
-		new SpringApplicationBuilder()
-				.sources(WicketApplication.class)
-				.run(args);
+		SpringApplication.run(WicketApplication.class, args);
 	}
 }
